57f78791f7ff4bd14f49d85c8048ef2a8221ee75
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 <lautaroolmedo77@gmail.com>
2024-12-19 12:25:35 -0300

    definition of architecture. Basic register user implementation

--- src/contexts/user/application/register-user-use-case/register-user.ts
+++ src/contexts/user/application/register-user-use-case/register-user.ts
@@ -1,8 +1,8 @@
-import { log } from 'console';
+import { Injectable } from '@nestjs/common';
+// -------------------- -------------------- --------------------
 import { User } from '../../domain/user';
 import { UserRepository } from '../../domain/user.repository';
 import { RegisterUserDTO } from './register-user.dto';
-import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class RegisterUser {
@@ -11,7 +11,6 @@ export class RegisterUser {
   public async execute(dto: RegisterUserDTO): Promise<void> {
     const user: User = User.create(dto); // ---> instancia del nuevo user
     console.log('user: ', user);
-
     await this.userRepository.register(user);
   }
 }
